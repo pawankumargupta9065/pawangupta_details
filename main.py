a533e1c5a39a8ccad3dded2ae3330c8ad12d3c89
pawankumargupta9065 <156783791+pawankumargupta9065@users.noreply.github.com>
2024-01-16 16:03:49 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pickle
 
 
 # Load the trained model
-with open('trained_model.pkl', 'rb') as model_file:
+with open('trained_model_lr.pkl', 'rb') as model_file:
     trained_model = pickle.load(model_file)
 
 st.title('Titanic Survival Prediction')
@@ -30,7 +30,12 @@ if st.button('Predict'):
     # Make predictions using the loaded model
     predicted_class = trained_model.predict(input_features_array)
 
+    print(predicted_class)
 
-    
-    
-st.write(f"Predicted class: {survival_dict.get(predicted_class[0])}")
+
+if predicted_class[0]==0:
+    print('person will die')
+else:
+    print('The Person will Survive.')
+
+    st.write(f"Predicted class: {predicted_class[0]}")
